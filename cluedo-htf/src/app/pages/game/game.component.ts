33cd46550ba939688288490db7d2daeb48f045eb
Amaury <amauryverschooren@icloud.Com>
2021-12-02 15:26:20 +0100

    reth

--- cluedo-htf/src/app/pages/game/game.component.ts
+++ cluedo-htf/src/app/pages/game/game.component.ts
@@ -7,17 +7,21 @@ import { CluedoService } from 'src/app/services/cluedo.service';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
-  
+  round: number = 0;
   constructor(private cluedoService: CluedoService) { }
   
   ngOnInit(): void {
+    this.createLobby();
   }
+  updateRound(){
 
+  }
   
   createLobby(){
     this.cluedoService.createGame().subscribe((resp)=>{
       this.cluedoService.lobby = resp;
       console.log(this.cluedoService.lobby);
+      this.createGame();
     })
   }
   createGame(){
